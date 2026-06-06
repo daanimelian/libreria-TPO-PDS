53b68de27d0eb78c93d71e3e3bea6f1391187b98
Daniela Melian <meliandaani@gmail.com>
2026-06-06 18:19:45 -0300

    logica de los metodos

--- src/src/MercadoPago.java
+++ src/src/MercadoPago.java
@@ -1,2 +1,29 @@
-public class MercadoPago {
+public class MercadoPago implements PaymentStrategy {
+
+    private String cuentaEmail;
+    private String accessToken;
+
+    public MercadoPago(String cuentaEmail, String accessToken) {
+        this.cuentaEmail = cuentaEmail;
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public boolean procesar(double monto) {
+        if (!validarDatos() || monto <= 0) return false;
+        System.out.println("Procesando pago con MercadoPago por $" + monto + " desde " + cuentaEmail);
+        return true;
+    }
+
+    @Override
+    public String obtenerDetalles() {
+        return "MercadoPago | Cuenta: " + cuentaEmail;
+    }
+
+    @Override
+    public boolean validarDatos() {
+        boolean emailValido = cuentaEmail != null && cuentaEmail.matches("^[\\w.+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}$");
+        boolean tokenValido = accessToken != null && !accessToken.isBlank();
+        return emailValido && tokenValido;
+    }
 }
